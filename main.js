75acf556d26affc5f7d8cf0d1a4934d749702be4
AnikaTahashin10 <anikatahashin90@gmail.com>
2023-10-29 20:40:03 +0200

    added the searchBar Function

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
-const search = document.getElementById('search'); 
+const search = document.getElementById('searchBar'); 
 const pokedex = document.getElementById('pokedex');
+let pokemon = [];
+
 console.log(pokedex);
+
+//to make the searchbar work
+console.log(searchBar);
+searchBar.addEventListener('keyup', (e) => {
+  const searchString = (e.target.value.toLowerCase());
+  const filteredCharacters = pokemon.filter(pokeman => {
+    return pokeman.name.toLowerCase().includes(searchString);
+  });
+  displayPokemon(filteredCharacters )
+});
 
 
 const fetchpokemon = () => {
   const promises = [];
-  for (i = 1; i<= 150; i++ ){
-const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+  for (i = 1; i<= 150 ; i++ ){
+const url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
 promises.push(fetch(url).then((res) => res.json()))
   }
 
   Promise.all(promises).then((results) => {
-    const pokemon = results.map((data) => ({
+     pokemon = results.map((data) => ({
       id: data.id,
       name: data.name,
       image: data.sprites['front_default'],
       type: data.types.map((type) => type.type.name).join(', '),
       weight: data.weight,
       height: data.height,
+      //generation: data.generation.name,
     }));
    displayPokemon(pokemon);
   });
@@ -34,7 +47,147 @@ const displayPokemon = (pokemon) => {
   </li>`).join('');
   pokedex.innerHTML = pokemonHTMLString;
 };
+
  fetchpokemon()
+
+
+
+
+
+
+ 
+
+
+
+
+
+//  document.addEventListener("DOMContentLoaded", function () {
+//   const imgOptions = {};
+//   const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+//     entries.forEach((entry) => {
+//       if (!entry.isIntersecting) return;
+//       const img = entry.target;
+//       const dataImage = img.getAttribute("data-image");
+//       img.src = dataImage;
+//       imgObserver.unobserve(img);
+//     });
+//   }, imgOptions);
+
+//   const orderNumber = (str) => {
+//     const mySubString = str.substring(str.lastIndexOf("s/") + 2, str.lastIndexOf("/"));
+//     return mySubString;
+//   };
+
+//   const container = document.getElementById("container");
+
+//   const fetchPokemons = async (endpoint) => {
+//     try {
+//       const response = await fetch(endpoint, {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       });
+//       const data = await response.json();
+//       return data.pokemon_species;
+//     } catch (error) {
+//       console.log(error);
+//       return [];
+//     }
+//   };
+
+//   const updateImageClass = (img) => {
+//     const imagelink = img.parentNode;
+//     imagelink.classList.add("loaded");
+//   };
+
+//   const createPokemonItem = (pokemon, toggleurl) => {
+//     const numero3decimals = orderNumber(pokemon.url);
+//     const paddedNumber = numero3decimals.padStart(3, "0");
+//     const imageUrl = `${toggleurl}${paddedNumber}.png`;
+
+//     const divitem = document.createElement("li");
+//     divitem.classList.add("item");
+
+//     const img = new Image();
+//     img.src = "https://i.gifer.com/origin/28/2860d2d8c3a1e402e0fc8913cd92cd7a_w200.gif";
+//     img.setAttribute("data-image", imageUrl);
+//     img.setAttribute("class", "pokeimage");
+//     img.setAttribute("alt", pokemon.name);
+
+//     divitem.innerHTML = `
+//       <div>${orderNumber(pokemon.url)} - ${pokemon.name}</div>`;
+//     divitem.appendChild(img);
+//     container.appendChild(divitem);
+//     imgObserver.observe(img);
+//   };
+
+//   const getPokemons = async (numero, toggle) => {
+//     const endpoint = `https://pokeapi.co/api/v2/generation/${numero}/`;
+//     container.innerHTML = "";
+//     const pokemons = await fetchPokemons(endpoint);
+    
+//     const toggleurl = toggle
+//       ? "https://assets.pokemon.com/assets/cms2/img/pokedex/full/"
+//       : "https://www.serebii.net/pokemongo/pokemon/";
+
+//     pokemons.sort((a, b) => a.nr - b.nr);
+
+//     pokemons.forEach((pokemon) => {
+//       createPokemonItem(pokemon, toggleurl);
+//     });
+//   };
+
+  // const filters = document.getElementById("filters");
+  // const geners = [
+  //   "generation-1",
+  //   "generation-2",
+  //   "generation-3",
+  //   "generation-4",
+  //   "generation-5",
+  //   "generation-6",
+  //   "generation-7",
+  // ];
+
+//   let gen = "";
+//   for (let i = 0; i < geners.length; i++) {
+//     gen += `
+//       <input class="radio-gens" type="radio" id=${geners[i]} value=${i + 1} name="generation" checked>
+//       <label for=${geners[i]} class="label-gens">${geners[i]}</label>`;
+//   }
+//   filters.innerHTML = gen;
+
+//   filters.addEventListener("click", function (e) {
+//     if (e.target.type === "radio") {
+//       getPokemons(e.target.value, toggle);
+//       title.innerHTML = "Pokemon " + e.target.id;
+//     }
+//   });
+
+//   const btnAllSchool = document.getElementById("btnAllSchool");
+//   let toggle = false;
+//   btnAllSchool.addEventListener("click", function () {
+//     toggle = !toggle;
+//     getPokemons(numero, toggle);
+//   });
+
+//   const title = document.getElementById("title");
+//   const numero = 1;
+
+//   getPokemons(numero);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
